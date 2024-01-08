fd9260c03c4253c0786c115b03fc95d6f176bbe7
UrmishaDas <ud.rocks13@gmail.com>
2024-01-09 00:27:38 +0530

    added fun 2

--- hoc/src/setupTests.js
+++ hoc/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+function fun2() {
+    var a = 5
+    console.log('Hello Chandler')
+}
